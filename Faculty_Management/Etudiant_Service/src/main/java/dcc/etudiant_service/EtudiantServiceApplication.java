a9a0d4fb62fdf4a8a675980096d4c2fb8f4bfd66
FatimaGhi <fatimazahraeelghiouan69@gmail.com>
2025-12-01 21:21:00 +0100

    first commit

---
+++ Faculty_Management/Etudiant_Service/src/main/java/dcc/etudiant_service/EtudiantServiceApplication.java
@@ -0,0 +1,19 @@
+package dcc.etudiant_service;
+
+import dcc.etudiant_service.configuration.RsaKeys;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableConfigurationProperties(RsaKeys.class)
+@EnableFeignClients
+@EnableCaching
+public class EtudiantServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EtudiantServiceApplication.class, args);
+    }
+
+}
---
+++ Faculty_Management/Etudiant_Service/src/main/java/dcc/etudiant_service/FeignClient/FiliereClient.java
@@ -0,0 +1,59 @@
+package dcc.etudiant_service.FeignClient;
+
+
+import dcc.etudiant_service.DTO.Filiere;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@FeignClient(
+        name = "Filiere-Service",
+        url = "http://localhost:8081/v1/filieres" // URL de  microservice filiere
+)
+@CircuitBreaker(name = "getfilier",fallbackMethod = "Filiere_fallbackMethod")
+//@TimeLimiter(name = "getfilier",fallbackMethod ="Filiere_fallbackMethod")
+//@RateLimiter(name = "getALLfilier",fallbackMethod ="Filiere_fallbackMethod_GA")
+//@Retry(name = "getALLfilier",fallbackMethod ="Filiere_fallbackMethod_GA")
+//@Bulkhead(name = "getALLfilier",fallbackMethod ="Filiere_fallbackMethod_GA")
+
+//@Cacheable(value = "filiere-cache", key = "#id")
+public interface FiliereClient {
+
+    @GetMapping("/{id}")
+    public Filiere getFiliereById(@PathVariable("id") Integer id);
+
+    @GetMapping
+    public List<Filiere> getAll();
+
+
+    default Filiere Filiere_fallbackMethod(Integer id,Exception e){
+        return new Filiere(id ,"No_exist","No_exist");
+    }
+    default List<Filiere> Filiere_fallbackMethod_GA(Integer id,Exception e){
+        return List.of(new Filiere());
+    }
+
+
+
+//    default List<Filiere> Filiere_fallbackMethod_GA(Integer id){
+//
+//        List<Filiere> filieres = new ArrayList<>();
+//        filieres = FiliereClient.getAll();
+//        return filieres;
+//
+//    }
+
+
+
+
+
+}

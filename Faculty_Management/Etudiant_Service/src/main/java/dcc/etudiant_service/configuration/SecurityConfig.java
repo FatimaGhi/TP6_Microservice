be7b2ac4f0f513fefef38a4878af0610674f141f
FatimaGhi <fatimazahraeelghiouan69@gmail.com>
2025-12-22 23:28:32 +0100

    dskip test in config  student

--- Faculty_Management/Etudiant_Service/src/main/java/dcc/etudiant_service/configuration/SecurityConfig.java
+++ Faculty_Management/Etudiant_Service/src/main/java/dcc/etudiant_service/configuration/SecurityConfig.java
@@ -2,6 +2,7 @@ package dcc.etudiant_service.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,7 +12,7 @@ import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-
+@Profile("!test")
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)

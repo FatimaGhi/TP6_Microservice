0a216e951db43d21bb373f76766efd090baf16dc
FatimaGhi <fatimazahraeelghiouan69@gmail.com>
2025-12-22 22:40:21 +0100

    fake data in student

--- Faculty_Management/Etudiant_Service/src/test/java/dcc/etudiant_service/EtudiantServiceApplicationTests.java
+++ Faculty_Management/Etudiant_Service/src/test/java/dcc/etudiant_service/EtudiantServiceApplicationTests.java
@@ -2,8 +2,10 @@ package dcc.etudiant_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class EtudiantServiceApplicationTests {
 
     @Test

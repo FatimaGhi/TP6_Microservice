8ef2e5500166048c501ab15df59256afb341de3b
FatimaGhi <fatimazahraeelghiouan69@gmail.com>
2025-12-22 21:39:22 +0100

    add fake data

--- Faculty_Management/Filiere_Service/src/main/java/dcc/filiere_service/FiliereServiceApplication.java
+++ Faculty_Management/Filiere_Service/src/main/java/dcc/filiere_service/FiliereServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 
 @SpringBootApplication
 @EnableConfigurationProperties(RsaKeys.class)
+@ActiveProfiles("test")
 public class FiliereServiceApplication {
 
     public static void main(String[] args) {
